8b9f00fe9b2cfeeaa30086d45356630fe84281c2
JWEi <jwei19943@gmail.com>
2024-04-02 18:09:36 +0800

    feat: Enable AspectJ Auto Proxy and Caching

--- src/main/java/com/ems/employeemanagement/EmployeemanagementApplication.java
+++ src/main/java/com/ems/employeemanagement/EmployeemanagementApplication.java
@@ -2,8 +2,12 @@ package com.ems.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
+@EnableCaching
 public class EmployeemanagementApplication {
 
     public static void main(String[] args) {
