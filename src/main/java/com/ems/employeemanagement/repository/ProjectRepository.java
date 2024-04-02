252f5aa4b63e6839ac41d02f9dd9ed7b08209100
JWEi <jwei19943@gmail.com>
2024-04-02 14:48:42 +0800

    feat: Add repository for Employee, Department, Project entity

---
+++ src/main/java/com/ems/employeemanagement/repository/ProjectRepository.java
@@ -0,0 +1,9 @@
+package com.ems.employeemanagement.repository;
+
+import com.ems.employeemanagement.model.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+}
